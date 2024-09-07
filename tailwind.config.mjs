751b3ca20483f58d89dc3867e20125fc214fd364
Michele Memoli <michele.memoli@gmail.com>
2024-09-07 17:18:20 +0100

    feat(app): blog styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fancy: ["Ga Maamli", ...defaultTheme.fontFamily.sans],
+        sans: defaultTheme.fontFamily.sans,
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
